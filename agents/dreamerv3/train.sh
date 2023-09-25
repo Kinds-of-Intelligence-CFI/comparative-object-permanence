4173c03bde512512e59fb1cce75d2034548ca07a
Wout Schellaert <wout.schellaert@gmail.com>
2023-09-25 10:42:11 +0200

    Add yaml random split in parts

---
+++ agents/dreamerv3/train.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICE=1 xvfb-run -a python train.py \
+  --task ../../configs/tests_agents/op_controls_50percent_part1.yml \
+  --aai ../../env/env3.1.2.exp/AAI.x86_64 \
+  --from-checkpoint ./logdir/training-2023_09_18_14_28-basic_controls/checkpoint.ckpt
--- agents/src/yamlHandling.py
+++ agents/src/yamlHandling.py
@@ -11,6 +11,7 @@ Animal-AI Version: 3.0.2
 import fnmatch
 import os
 import random
+from pathlib import Path
 
 def find_yaml_files(directory):
     yaml_files = []
@@ -23,26 +24,38 @@ def find_yaml_files(directory):
     
     return yaml_files, task_names
 
-def yaml_combinor(file_list: list, temp_file_location: str, stored_file_name = "TempConfig_0.yml", sample = 1.0):
+def yaml_combinor(file_list: list, tmp_file_path: Path, shuffle = True):
     """
     Provide a list of paths to instances and the place you want to store the temporary file. You can change the name of the temporary file.
     """
-    temp_file_path = os.path.join(temp_file_location, stored_file_name)
-    # Only sample a certain percentage of the files
-    file_list = random.sample(file_list, int(len(file_list) * sample))
+    if shuffle:
+        file_list = random.sample(file_list, len(file_list)) # Should be without replacement
     try:
-        with open(temp_file_path, 'w') as output_file:
+        with open(tmp_file_path, 'w') as output_file:
             output_file.write("!ArenaConfig\narenas:\n")
             for i, file in enumerate(file_list):
                 with open(file, 'r') as input_file:
                     lines = input_file.readlines()[2:] #skip the first two lines that contain `!ArenaConfig\narenas:\n`
                     lines[0] = lines[0].replace('0: !Arena', str(i) + ": !Arena").replace('-1: !Arena', str(i) + ": !Arena") #make sure to enumerate the arena objects properly
                     output_file.writelines(lines)
-        print(f"Yaml files combined. Saved to {temp_file_path}")
-        return temp_file_path
+        print(f"Yaml files combined. Saved to {tmp_file_path}")
+        return tmp_file_path
     except IOError as e:
         print(e)
         print("An error occurred while combining files.")
+
+def yaml_combine_in_parts(file_list: list, tmp_file_path: Path, n_parts: int = 3, shuffle = True):
+    if shuffle:
+        file_list = random.sample(file_list, len(file_list)) # Should be without replacement
+    parts = partition(file_list, n_parts)
+    for i, part in enumerate(parts, 1):
+        yaml_combinor(part, tmp_file_path.with_stem(f"{tmp_file_path.stem}_part{i}"), shuffle = shuffle)
+
+# https://stackoverflow.com/questions/2659900/slicing-a-list-into-n-nearly-equal-length-partitions
+def partition(lst, n):
+    division = len(lst) / n
+    return [lst[round(division * i):round(division * (i + 1))] for i in range(n)]
+
 
 if __name__ == "__main__":
     from pathlib import Path
@@ -51,8 +64,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=Path, required=True)
     parser.add_argument('--temp-file', type=Path, required=True)
-    parser.add_argument('--sample', type=float, default=1.0)
+    parser.add_argument('--shuffle', action='store_true')
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--n-parts', type=int, default=None)
     args = parser.parse_args()
 
+    random.seed(args.seed)
     yaml_files, task_names = find_yaml_files(args.dir)
-    yaml_combinor(yaml_files, str(args.temp_file.parent), args.temp_file.name, args.sample)
+    if args.n_parts is not None:
+        yaml_combine_in_parts(yaml_files, args.temp_file, args.n_parts, args.shuffle)
+    else:
+        yaml_combinor(yaml_files, args.temp_file, args.shuffle)
